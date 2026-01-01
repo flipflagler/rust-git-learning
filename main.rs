2d89ac8ff95d8389345b721f03efc607ff85b8ca
Harsh <harshgi200@gmail.com>
2026-01-01 15:02:23 +0530

    Q1: move vs copy

--- main.rs
+++ main.rs
@@ -1,4 +1,8 @@
 fn main (){
-    print!("rust learning playground    ");
+    let a : i32 =123;
+    let b = a;
+    let a1 = String::from ("hello");
+    let a2 = &a1;
+    println!("a{}b{}a1{}a2{}",a,b,a1,a2);
     
 }
