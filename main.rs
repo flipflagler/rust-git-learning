e85ff85728893ff1e767afb12b5b60f2829c5d0c
Harsh <harshgi200@gmail.com>
2026-01-01 15:44:47 +0530

    Fix the Move Error

--- main.rs
+++ main.rs
@@ -1,8 +1,9 @@
-fn main (){
-    let a : i32 =123;
-    let b = a;
-    let a1 = String::from ("hello");
-    let a2 = &a1;
-    println!("a{}b{}a1{}a2{}",a,b,a1,a2);
-    
+fn main() {
+    let s = String::from("rust");
+    takes_ownership(&s);
+    println!("{}", s);
+}
+
+fn takes_ownership(x: &String) {
+    println!("{}", x);
 }
