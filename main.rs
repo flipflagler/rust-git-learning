45d0d2c4123ae442bc72d83f41c215efbcd836bf
Harsh <harshgi200@gmail.com>
2026-01-04 00:31:02 +0530

    iter collect

--- main.rs
+++ main.rs
@@ -1,9 +1,10 @@
 fn main (){
     let  v1 = vec![1,2,3,4];
     let iter = v1.iter();
-    let iter2 =  iter.filter(|x| *x%2 == 0);
-    for x in iter2 {
-        println!("{}",x);
-    }
-   println!("{:?}",v1);
+    let iter2 =  iter.filter(|x| *x%2 != 0).map(|x| x*2);
+    let v2:Vec<i32> = iter2.collect();
+    // for x in iter2 {
+    //     v2.push(x);
+    // }
+   println!("{:?}",v2);
 }
