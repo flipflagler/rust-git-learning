b0063e3e780b4a10a4b4fd055745c4958d8cbc0f
Harsh <harshgi200@gmail.com>
2026-01-04 00:14:33 +0530

    iter map

--- main.rs
+++ main.rs
@@ -1,8 +1,9 @@
 fn main (){
-    let mut v1 = vec![1,2,3,];
-    let mut v1_iter = v1.iter_mut();
-   while let Some(val)= v1_iter.next(){
-    *val +=10;
-   }
-   println!("{:?}",v1);
+    let  v1 = vec![1,2,3,];
+    let iter = v1.iter();
+    let iter2 =  iter.map(|x| x + 1);
+    for x in iter2 {
+        println!("{}",x);
+    }
+//    println!("{:?}",v1);
 }
