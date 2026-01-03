7eb283d9e8ae122512cb80bb0ea15cb960dcb65c
Harsh <harshgi200@gmail.com>
2026-01-03 20:04:11 +0530

    iter

--- main.rs
+++ main.rs
@@ -1,15 +1,7 @@
-use std:: collections::HashMap;
-
-fn group_values_by_key (vec: Vec<(String,i32)>) -> HashMap<String,i32>{
-    let mut hm = HashMap:: new();
-    for (key ,value) in vec{
-        hm.insert(key,value);
+fn main (){
+    let v1 = vec![1,2,3,];
+    let v1_iter = v1.iter();
+    for val in v1_iter{
+        println!("got:{val}");
     }
-    return hm;
-}
-
-fn main(){
-    let input_vec = vec![(String::from("harsh"),20),(String::from("raman"),30)];
-    let hm =  group_values_by_key(input_vec);
-    print!("{:?}",hm);
 }
